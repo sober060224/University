8eee830da91dc1ae217c68f5c7db36cd3ece14de
sober060224 <2680038634@qq.com>
2026-02-01 18:32:27 +0800

    algorithm1-2 sort

--- C++/Example/hello.c
+++ C++/Example/hello.c
@@ -1,7 +1,16 @@
+#include <string.h>
 #include <stdio.h>
 
 int main()
 {
+    int rule;
+    char argv[100] = "0x1808";
+    for (int i = 2; i < strlen(argv); i++)
+    {
+        rule *= 16;
+        rule += (argv[i] - '0');
+    }
+    printf("%d", rule);
 
     return 0;
 }
